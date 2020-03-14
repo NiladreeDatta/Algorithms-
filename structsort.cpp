c5c5f9c609d5c81037a5b60d420d48dc2c47c180
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-03-14 05:02:56 -1200

    Add files via upload

---
+++ structsort.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+
+const int mx = 1e5+123;
+
+bool cmp ( const pii &a, const pii &b )
+{
+    if ( a.F > b.F ) return 1;
+    if ( a.F == b.F ) return ( a.S <= b.S );
+    return 0;
+}
+
+int main()
+{
+    optimize();
+
+    vii v;
+    v.PB ( { 2, 3 } );
+    v.PB ( { 4, 120 } );
+    v.PB ( { 4, 220 } );
+    v.PB ( { 3, 50 } );
+
+    sort ( all ( v ), cmp );
+
+
+    for ( auto u : v ) cout << u.F << " " << u.S << endl;
+
+
+    return 0;
+}

9cb49ef0145ec0a644e928bcf1e5ab305d789e77
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-07-11 20:13:18 +0600

    Add files via upload

---
+++ Subtree size using DFS on a tree.cpp
@@ -0,0 +1,15 @@
+void dfs(int node)
+{
+    vis[node] = 1;
+    int cur_size = 1;
+    for(int child : adj[node])
+    {
+        if(!vis[child])
+        {
+            cur_size += dfs(child);
+        }
+    }
+    subTree[node] = cur_size;
+    return cur_size;
+
+}
